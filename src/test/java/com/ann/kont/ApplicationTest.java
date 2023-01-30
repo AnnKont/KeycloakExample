d0ad664901fb8dc40fa48faf35c710f5803c0d36
AnnKont <arb9i14@gmail.com>
2023-01-30 21:01:46 +0500

    add git hub actions

---
+++ src/test/java/com/ann/kont/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.ann.kont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
